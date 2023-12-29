e42f32d3f4f15a78145e8548d686b3b83f85673f
laila1996 <lmajidi@students.pccc.edu>
2023-12-29 00:43:24 -0500

    everything set up

---
+++ src/test/java/com/projectByMe/runners/FailedTestRunner.java
@@ -0,0 +1,14 @@
+package com.projectByMe.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/cydeo/step_definitions"
+)
+public class FailedTestRunner {
+}
---
+++ src/test/java/com/projectByMe/step_definitions/Hooks.java
@@ -0,0 +1,46 @@
+package com.projectByMe.step_definitions;
+
+import com.projectByMe.utility.ConfigurationReader;
+import com.projectByMe.utility.Driver;
+import com.projectByMe.utility.DB_Util;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.util.concurrent.TimeUnit;
+
+public class Hooks {
+
+    @Before("@ui")
+    public void setUp(){
+        Driver.getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        Driver.getDriver().manage().window().maximize();
+        Driver.getDriver().get(ConfigurationReader.getProperty("library_url"));
+
+    }
+
+    @After("@ui")
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()){
+            final byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot,"image/png","screenshot");
+        }
+
+        Driver.closeDriver();
+
+    }
+
+    @Before("@db")
+    public void setUpDB(){
+        System.out.println("Connecting to database...");
+        DB_Util.createConnection();
+    }
+
+    @After("@db")
+    public void tearDownDB(){
+        System.out.println("close database connection...");
+        DB_Util.destroy();
+    }
+}
